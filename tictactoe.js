b94772e41e1a319785539fc86336ae11d7968d84
Darma <meet.darma@gmail.com>
2026-01-20 16:09:09 +0700

    Add the gameOver to stop the game and force to reset when there is a winner or the game resulted draw.

--- tictactoe.js
+++ tictactoe.js
@@ -4,6 +4,7 @@ const ttt = (function() {
 
     let board;
     let currentPlayer;
+    let gameOver;
     const winPattern = [
         [0,1,2], [3,4,5], [6,7,8],
         [0,3,6], [1,4,7], [2,5,8],
@@ -21,11 +22,16 @@ ${board[6]}  |  ${board[7]}  |  ${board[8]}
         }
     
     function play(num) {
+        if (gameOver) {
+            console.log('The game is Over. Please reset')
+            return;
+        }
         if ( num < 0 || num > 8 ||
             board[num] === 'X' ||
             board[num] === 'O'
         ) {
             console.log('Invalid move!')
+            return;
         }
         move(num)
 
@@ -52,6 +58,8 @@ ${board[6]}  |  ${board[7]}  |  ${board[8]}
         if (board[a] === board[b] &&
             board [b] === board[c]) {
             console.log(`${currentPlayer} is the winner`)
+            gameOver = true
+            return;
         } else {
             null
         }
@@ -59,6 +67,8 @@ ${board[6]}  |  ${board[7]}  |  ${board[8]}
 
     function checkDraw() {
         return board.every(b => b === 'X' || b === 'O')
+        gameOver =  true;
+        return;
     }
 
     function switchPlayer() {
@@ -68,6 +78,7 @@ ${board[6]}  |  ${board[7]}  |  ${board[8]}
     function reset() {
         board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
         currentPlayer = "X";
+        gameOver = false;
         showBoard()
     }
 
